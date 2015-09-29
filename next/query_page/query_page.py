e83aa8cc44a6a4009037916f24dfcf0d3bef65b7
scottsievert <sieve121@umn.edu>
2015-09-28 21:51:34 -0500

    closes #5 and allows user to specify number of queries

--- next/query_page/query_page.py
+++ next/query_page/query_page.py
@@ -4,15 +4,21 @@ from flask.ext.login import login_user, logout_user, login_required, current_use
 from next.api.keychain import KeyChain
 import next.constants as constants
 import os
+from next.api.resource_manager import ResourceManager
+resource_manager = ResourceManager()
 
 keychain = KeyChain()
 query_page = Blueprint('query_page', __name__, template_folder="templates", static_folder="static")
 
-@query_page.route('/query_page/<page>') 
-@query_page.route('/query_page/<page>/<exp_uid>/<widget_key>') 
+@query_page.route('/query_page/<page>')
+@query_page.route('/query_page/<page>/<exp_uid>/<widget_key>')
 def load_page(page, exp_uid=None, widget_key=None):
+    experiment = resource_manager.get_experiment(exp_uid)
+    num_tries = experiment['num_tries']
     app_template = page+'.html'
     print "app template is %s"%(app_template)
     host_url = "http://"+constants.NEXT_BACKEND_GLOBAL_HOST+":"+constants.NEXT_BACKEND_GLOBAL_PORT
-    return render_template(app_template, host_url=host_url, exp_uid=exp_uid, widget_key=widget_key), 200, {'Cache-Control':'private, max-age=0, no-cache, no-store'}
+    return render_template(app_template, host_url=host_url, exp_uid=exp_uid,
+                           widget_key=widget_key, num_tries=num_tries), \
+            200, {'Cache-Control':'private, max-age=0, no-cache, no-store'}
 
